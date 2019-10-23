7f8d179db7902882de7f8662964d8113e6fec744
SimonG <saguti15@g.holycross.edu>
2019-10-23 17:20:59 -0400

    finished loops

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,8 +1,57 @@
 'use strict';
 
+
+// let nextTracker = {};
+// //Complete this algo
+// const isLoop = (linkedlist) => {
+
+//     let currNode;
+
+//     if (linkedlist.head) {
+//         currNode = linkedlist.head;
+//     } else {
+//         currNode = linkedlist.next;
+//     }
+//     if (!currNode.next) {
+//         return false;
+//     }
+
+//     let nextVal = currNode.next.value;
+
+//     if (nextTracker[nextVal]) {
+//        return true;
+//     } else {
+//         nextTracker[nextVal] = 1;
+//     }
+
+//     return isLoop(currNode);
+
+// };
+
+
 //Complete this algo
 const isLoop = (linkedlist) => {
+    let nextTracker = {};
+    let firstNode = linkedlist.head;
 
+    const helper = (node) => {
+        let currNode = node.next;
+        if (!currNode.next) {
+            return false;
+        }
+
+        let nextVal = currNode.next.value;
+
+        if (nextTracker[nextVal]) {
+        return true;
+        } else {
+            nextTracker[nextVal] = 0;
+        }
+
+        return helper(currNode);
+
+    }
+    return helper(firstNode);
 };
 
 
